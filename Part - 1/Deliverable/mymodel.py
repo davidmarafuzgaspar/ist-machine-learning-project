04ac1f865b2a9061a688c793c4307e1742a5bf57
pedromonico-dev <pedrogmonico.dev@gmail.com>
2025-10-06 23:37:16 +0100

    bora bora

--- Part - 1/Deliverable/mymodel.py
+++ Part - 1/Deliverable/mymodel.py
@@ -5,8 +5,8 @@ import joblib
 model = joblib.load("champion.pkl")
 
 def predict(Xtest):
-    # Remove the first sensor (index 0) -> same as training
-    Xtest_mod = np.delete(Xtest, 0, axis=1)
+    # Remove the second sensor (index 1)
+    Xtest_mod = np.delete(Xtest, 1, axis=1)
     # Predict with pipeline
     y_pred = model.predict(Xtest_mod)
     
